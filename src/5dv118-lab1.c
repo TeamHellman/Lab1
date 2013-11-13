c30ed526f3430c257cb3cddf05558df7b0afe183
oi11hms <oi11hms@humankapital.cs.umu.se>
2013-11-13 15:56:44 +0100

    asd

--- src/5dv118-lab1.c
+++ src/5dv118-lab1.c
@@ -7,12 +7,14 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello Wssssorld!!! */
 	/* fyfanasda*/
-	return EXIT_SUCCESS;
+	int boris = 2;
+	printf("%d", boris);
+
+	return 1;
 }
